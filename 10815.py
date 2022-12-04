7d864f63d72d9d5e97a259bf2123f545246461ae
noeyiz <lamoureclatant@gmail.com>
2022-12-04 16:24:35 +0900

    10815번: 숫자 카드

--- 10815.py
+++ 10815.py
@@ -3,12 +3,13 @@
 ###
 
 import sys
+input = sys.stdin.readline
 
 # 입력값 받기
-n = int(sys.stdin.readline())
-cards = list(map(int, sys.stdin.readline().split()))
-m = int(sys.stdin.readline())
-nums = list(map(int, sys.stdin.readline().split()))
+n = int(input())
+cards = list(map(int, input().split()))
+m = int(input())
+nums = list(map(int, input().split()))
 
 # 이분 탐색을 위해 정렬
 cards.sort()
@@ -19,6 +20,7 @@ def binary_search(target, start, end):
         return None
     
     # 중간값 찾기
+    ### //: 몫을 구하는 산술 연산자입니다 ~
     mid = (start + end) // 2
 
     # 원하는 값이면 반환
@@ -33,6 +35,8 @@ def binary_search(target, start, end):
 
 for i in range(m):
     if binary_search(nums[i], 0, n - 1) != None:
+        ### Python의 print 함수는 자동으로 개행이 붙기 때문에 
+        ### end 옵션을 사용해 개행 대신 ' '를 붙여줬습니다 ~
         print("1", end = ' ')
     else:
         print("0", end = ' ')
