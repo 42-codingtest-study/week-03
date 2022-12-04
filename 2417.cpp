a02d3dc7654239a9bb8f372622b0b949fee2a6d0
donghyun1998 <donghyun1998@naver.com>
2022-12-04 16:01:10 +0900

    no sqrtfunc ok

--- 2417.cpp
+++ 2417.cpp
@@ -1,35 +1,36 @@
 //정수 제곱근
-
+//sqrt함수 사용 시 오류 남
 #include <bits/stdc++.h>//온갖 헤더 다때려박음
 #include <iostream>
 using namespace	std;
 //파일 입력 : cin >> 변수명;
 //파일 출력 : cout << "a + "<< 변수 << " = c";
 //출력) a + 변수 = c
-void	ft_binary_search(long long n)
-{
-	long long start = 0, sqrtn = sqrt(n), end = sqrtn, mid;
-	while (start <= end)
-	{
-		mid = (start + end) / 2;
-		if (mid >= sqrtn)//루트값이랑 미드 같아질때까지
-			end = mid - 1;
-		else
-			start = mid + 1;
-	}
-	 if(mid * mid == n)
-		cout << mid;
-	else
-		cout << mid + 1;//외 않 됨????
-}
 
 int	main()
 {
 	ios::sync_with_stdio(false);//c++stream과 cstream의 연결 끊기 : 시간 단축
 	cin.tie(nullptr); //cin 전에 cout 버퍼 비우지 않게
 
-	long long n;
+	long long n, start, end, mid;
 
 	cin >> n;
-	ft_binary_search(n);// 걍 sqrt(n)+ 1 출력하니 오버플로우일어남
+	start = 0;
+	end = n;
+	while (start <= end)
+	{
+		mid = (start + end) / 2;
+		if (mid == 0 || mid == n / mid)// mid == 0일때 빼주기
+			break;
+		else if (mid > n / mid)
+			end = mid - 1;
+		else if (mid < n / mid)
+			start = mid + 1;
+	}
+	if (mid * mid == n)
+		cout << mid;
+	else if (mid == 0 || mid == n / mid)
+		cout << mid + 1;
+	else
+		cout << start;
 }
